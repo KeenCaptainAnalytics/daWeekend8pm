78a2a8de5e0e5703f521249a4f36fa4a1f55aa22
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-22 21:14:37 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,22 +1,49 @@
-def checkOtp():
-    print("==================")
-    print("chdceking otp")
-    print("user id chcek")
-    print("==================")
+Q1: juicer => 3 fruits
+#  1 => juice of fruits
+#  2 -> juice of fruit 1 and fruit2
+# 3 => juice of fruit1 and fruit2 and fruit3
+
+# def juicer(fruit1, fruit2="" ):
+#     print("get "+fruit1 + " - " +fruit2+" juice")
+#
+# juicer("banana","guava ")
+# juicer("orange", "grapes")
+# juicer("mango")
+#
 
 
-def sendOtp():
-    print("++++++++++++++++++++++++")
-    print("sending otp")
-    print("storing otp for match purpose")
-    print("++++++++++++++++++++++++")
+# def checkOtp(otp):
+#     print("==================")
+#     print(otp)
+#     print("chdceking otp")
+#     print("user id chcek")
+#     print("==================")
+#
+# # user 1 = 1234
+# # argument
+# checkOtp(1234)
+# # user 2  = 2345
+# checkOtp(2345)
+# # user3 = 5674
+# checkOtp(5674)
+# # user4 =9023
+# checkOtp(9023)
 
 
-
-sendOtp()
-sendOtp()
-checkOtp()
-checkOtp()
+#
+#
+# def sendOtp():
+#     print("++++++++++++++++++++++++")
+#     print("sending otp")
+#     print("storing otp for match purpose")
+#     print("++++++++++++++++++++++++")
+#
+#
+#
+# sendOtp()
+# sendOtp()
+# checkOtp()
+# checkOtp()
 
 
 # definition of a function
