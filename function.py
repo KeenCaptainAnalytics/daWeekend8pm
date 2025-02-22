aadf254e0e41c651dbcaad58f499dd290a25a67a
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-22 21:53:37 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,7 +1,49 @@
-Q1: juicer => 3 fruits
-#  1 => juice of fruits
+def generateOTP():
+    num = 7565
+    return num
+
+def match(usernum):
+    # match num = 
+
+
+
+# function add two number
+
+# def add():
+#     num1 = input("enter a number : ")
+#     num2 = input("enter a number : ")
+#     num1 = int(num1)
+#     num2 = int(num2)
+#     result= num1+num2
+#     return result
+# 
+# result1 =  add()
+# print(result1)
+# result2 =  add()
+# print(result2)
+
+# Q1: juicer => 3 fruits
+#  0 => you will not be given any juice
+#  1 => juice of fruit1
 #  2 -> juice of fruit 1 and fruit2
 # 3 => juice of fruit1 and fruit2 and fruit3
+#
+
+# def juicer(fruit1="", fruit2="", fruit3=""):
+#     if(fruit1=="" and fruit2=="" and fruit3==""):
+#         print("you will not be given any juice")
+#     elif(fruit2=="" and fruit3==""):
+#         print("juice of "+fruit1)
+#     elif(fruit3==""):
+#         print("juice of " + fruit1+" and "+fruit2)
+#     else:
+#         print("juice of " + fruit1 + " and " + fruit2+" and "+fruit3)
+#
+# juicer("", "plum", "lemon")
+# juicer("apple", "lemon")
+# juicer("apple")
+
+
 
 # def juicer(fruit1, fruit2="" ):
 #     print("get "+fruit1 + " - " +fruit2+" juice")
