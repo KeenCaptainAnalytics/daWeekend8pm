59ac5a314daac2be0cb8b26e8db3b656ceefd5d9
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-22 20:48:26 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,45 @@
-print("hello")
+def checkOtp():
+    print("==================")
+    print("chdceking otp")
+    print("user id chcek")
+    print("==================")
+
+
+def sendOtp():
+    print("++++++++++++++++++++++++")
+    print("sending otp")
+    print("storing otp for match purpose")
+    print("++++++++++++++++++++++++")
+
+
+
+sendOtp()
+sendOtp()
+checkOtp()
+checkOtp()
+
+
+# definition of a function
+# def printSomething():
+#     print("happy birthday")
+#     print("iam trying some new code ")
+#
+#
+# # //calling a function
+# print("=======================")
+# printSomething()
+# print("+++++++++++++++++++++")
+# printSomething()
+# for i in range (0,5):
+#     print("happy birthday")
+
+
+
+# i =0
+# while (i<5) :
+#     print("happy birthday")
+#     i= i+1
+# print("welcome")
+
+
+
