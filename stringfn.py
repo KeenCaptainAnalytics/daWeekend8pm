a35aada20db0acffa0778829671f8187002dc18c
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-02 21:27:14 +0530

    Update stringfn.py

--- stringfn.py
+++ stringfn.py
@@ -1,13 +1,62 @@
+num =89786   #=> 8
+num = str(num)
+
+r = num.count("8")
+print(r)
+
+# count= 0
+# while num!=0:
+#     if(num%10==8):
+#         count = count+1
+#     else:
+#         pass
+#
+#     num = num//10
+#
+# print(count)
+
+
+
 # string
+# s = "23yust2r ystry"
+#    # 0123456789
+# # r = s.count("str")
+# r = s.count("2", 4, 9)
+# print(r)
 
-s = "67 89 -0 54 23 909"
 
+
+# r = s.find("str")
+# r = s.find("str",5)
+# r = s.find("str",5, 10)
+# print(r)
+
+
+
+# s = "67 89 -0inkjm 54 23yuyyu  909 strhhg  iuyutg yuftftyf gygf 899 64 kjhjhgjg "
+
+
+# r = s.split()
+# print(r)
+#
+# sum =0
+# for i in range(0, len(r)):
+#     temp = r[i]
+#     if(temp.isdigit()==True):
+#         # print(temp)
+#         sum = sum +int(temp)
+#     else:
+#         pass
+    # num = int(r[i])
+    # sum = sum + num
+
+# print(sum)
 
 # how many times orange has appeared in the string
 # s = 'orange,yellow,red,black,green,Orange,red,ORANGE'
 # r = s.split(",")
 # print( r )
-# 
+#
 # count=0
 # for i in range (0, len(r)):
 #     l = r[i].lower()
@@ -15,7 +64,7 @@ s = "67 89 -0 54 23 909"
 #         count= count+ 1
 #     else:
 #         pass
-# 
+#
 # print("no of orange" ,count)
 
 
