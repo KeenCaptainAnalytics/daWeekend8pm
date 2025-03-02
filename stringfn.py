3e4e62ca96e8df33016029b819495a2f285606e4
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-02 20:50:28 +0530

    Create stringfn.py

---
+++ stringfn.py
@@ -0,0 +1,33 @@
+# string
+
+s = "67 89 -0 54 23 909"
+
+
+# how many times orange has appeared in the string
+# s = 'orange,yellow,red,black,green,Orange,red,ORANGE'
+# r = s.split(",")
+# print( r )
+# 
+# count=0
+# for i in range (0, len(r)):
+#     l = r[i].lower()
+#     if(l == "orange"):
+#         count= count+ 1
+#     else:
+#         pass
+# 
+# print("no of orange" ,count)
+
+
+# r = s.capitalize()
+# r = s.upper()
+# t = type(s)
+# r = s.lower()
+#
+# print(s, r)
+
+
+
+
+
+
