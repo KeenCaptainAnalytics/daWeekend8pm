60a7217c331b346e0baca3310219c8cd5b934c34
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-08 20:58:34 +0530

    Create string_fun2.py

---
+++ string_fun2.py
@@ -0,0 +1,35 @@
+s="abc8989,343,wqrwc,3453"
+
+8989+343+3453
+
+print(s.center(20," "))
+l= len(s)
+l= l//2
+print(s[l:l+1])
+
+# print(s.center(10,"="))
+
+# s.isnumeric()
+# s.isdigit()
+# s.isdecimal()
+# s.isdigit()
+# print(s.isalnum())
+# print(s.isdigit())
+# s = "abcd efghij"
+    #0123456789
+    #-10 , -9 .........-1
+    #  left to right
+    # step negative = > right to left
+# r= s[6:1:-1]
+
+
+# print(r)
+# output =  "gfedc"
+# slicing
+# r = s[4:10:3]
+# r= s[-3::-2]
+# first index => include
+# second index => excluded index
+
+# print("original string : " , s)
+# print("sliced string : ",r)
